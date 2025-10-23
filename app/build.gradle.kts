a4f475249dc43f881fba23f0793a9f07de3a943f
Jeff-Kidzie <jeffry.irmawan88@gmail.com>
2025-10-23 22:59:31 +0700

    add pdfviewer as library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.mhiew.pdfviewer)
+    implementation(libs.marain.pdfviewer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
